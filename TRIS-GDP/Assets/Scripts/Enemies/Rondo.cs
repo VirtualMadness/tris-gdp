2e1ad89f0c461059d899831289edb0b1116a9fa6
Pikas96 <sergio.yo.96@gmail.com>
2017-11-16 23:25:21 +0100

    Rondo y Brutus implementados, package 'AwesomeMovement' eliminado

---
+++ TRIS-GDP/Assets/Scripts/Enemies/Rondo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rondo : MonoBehaviour {
+
+	public Vector2 direction = new Vector2(1, 0);
+    public float speed = 4;
+
+	void Update () {
+        Vector2 aux = direction * speed * Time.deltaTime;
+		this.transform.position += new Vector3(aux.x, aux.y, 0);
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("RondoTrigger");
+        if(other.CompareTag("RondoTrigger"))
+        {
+            this.direction = this.direction*-1;
+        }
+    }
+}
